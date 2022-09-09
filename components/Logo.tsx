9c0f204c73e055f6ce1bbcb65681ec42f9e61518
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-09 16:08:27 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,17 @@
-export default function Logo({ ...rest }) {
+import Image from 'next/image'
+
+const myLoader = ({ src, width, quality }) => {
+  return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+}
+
+const MyImage = (props) => {
   return (
-     <Image
-      src="https://img.dinamalar.com/images/dmrenglishlogonew.png"
+    <Image
+      loader={myLoader}
+      src="me.png"
       alt="Picture of the author"
-      width={200}
-      height={51}
+      width={500}
+      height={500}
     />
-  );
+  )
 }
