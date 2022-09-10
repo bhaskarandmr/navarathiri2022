5d76a1882fecbbdf64521acd8b510aeca465af52
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-10 10:40:56 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,26 +41,7 @@ export default function Footer() {
   );
 }
 
-function FooterList({ title, items }: SingleFooterList) {
-  return (
-    <ListWrapper>
-      <ListHeader>{title}</ListHeader>
-      {items.map((singleItem) => (
-        <ListItem key={singleItem.href} {...singleItem} />
-      ))}
-    </ListWrapper>
-  );
-}
 
-function ListItem({ title, href }: SingleFooterListItem) {
-  return (
-    <ListItemWrapper>
-      <NextLink href={href} passHref>
-        <a>{title}</a>
-      </NextLink>
-    </ListItemWrapper>
-  );
-}
 
 const FooterWrapper = styled.div`
   padding-top: 10rem;
