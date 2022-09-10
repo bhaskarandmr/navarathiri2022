6c21457b6af8e3b2204c2f515e61baa494eeaa49
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-10 11:57:57 +0530

    Update ScrollableBlogPosts.tsx

--- views/HomePage/ScrollableBlogPosts.tsx
+++ views/HomePage/ScrollableBlogPosts.tsx
@@ -42,7 +42,7 @@ export default function ScrollableBlogPosts({ posts }: ScrollableBlogPostsProps)
                 <ArticleCard
                   title={singlePost.meta.title}
                   description={singlePost.meta.description}
-                  imageUrl={singlePost.imageUrl}
+                  imageUrl={singlePost.meta.imageUrl}
                   slug={singlePost.slug}
                 />
               </SwiperSlide>
