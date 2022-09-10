dd1b3226a313dff78f746496ffe9e2c20d987649
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-10 16:53:04 +0530

    Update ScrollableBlogPosts.tsx

--- views/HomePage/ScrollableBlogPosts.tsx
+++ views/HomePage/ScrollableBlogPosts.tsx
@@ -29,8 +29,8 @@ export default function ScrollableBlogPosts({ posts }: ScrollableBlogPostsProps)
     <Section>
       <Container>
         <Content>
-          <OverTitle>features</OverTitle>
-          <SectionTitle>What are you signing in for?</SectionTitle>
+          <OverTitle>நவராத்திரி </OverTitle>
+          <SectionTitle>செய்திகள்</SectionTitle>
         </Content>
       </Container>
 
