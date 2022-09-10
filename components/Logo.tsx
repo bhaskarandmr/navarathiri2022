892bc260fc4538cbbe182bf0e1b8002a63c8b821
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-10 15:29:10 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 function Avatar() {
-  return <Image src="/logo.png" alt="me" width="175" height="69" />
+  return <Image src="/logof.png" alt="me" width="175" height="69" />
 }
 
 export default Avatar
