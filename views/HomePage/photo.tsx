d3d161a532bc347032e42a42755da3da7081ca30
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-12 12:56:53 +0530

    Create photo.tsx

---
+++ views/HomePage/photo.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled from 'styled-components';
+import AutofitGrid from 'components/AutofitGrid';
+import BasicCard from 'components/BasicCard';
+import Container from 'components/Container';
+import SectionTitle from 'components/SectionTitle';
+
+import { media } from 'utils/media';
+
+const photo = [
+  {
+    imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+    imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+    imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+    imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+   imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+    imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+    imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+     imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+  {
+    imageUrl: '/1f.jpg',
+    title: 'நவராத்திரி நான்காம் நாள் பூஜை முறை',
+    description:
+      '',
+  },
+];
+
+
+export default function photo() {
+  return (
+    <Container>
+        <SectionTitle>photo</SectionTitle>
+      <CustomAutofitGrid>
+        {FEATURES.map((singleFeature, idx) => (
+          <BasicCard key={singleFeature.title} {...singleFeature} />
+        ))}
+      </CustomAutofitGrid>
+    </Container>
+  );
+  
+
+}
+
+const CustomAutofitGrid = styled(AutofitGrid)`
+  --autofit-grid-item-size: 40rem;
+
+  ${media('<=tablet')} {
+    --autofit-grid-item-size: 30rem;
+  }
+
+  ${media('<=phone')} {
+    --autofit-grid-item-size: 100%;
+  }
+`;
