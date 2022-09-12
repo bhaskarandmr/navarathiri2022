71760eb6f9ce9e770006118e920d394bf081d9ca
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-12 12:11:15 +0530

    Update photo.tsx

--- views/HomePage/photo.tsx
+++ views/HomePage/photo.tsx
@@ -9,27 +9,27 @@ export default class NextJsCarousel extends Component {
               <h2>NextJs Carousel - GeeksforGeeks</h2>
               <Carousel>
                   <div>
-                      <img src="/1.png" alt="image1"/>
+                      <img src="/1f.jpg" alt="image1"/>
                       <p className="legend">Image 1</p>
   
                   </div>
                   <div>
-                      <img src="/2.png" alt="image2" />
+                      <img src="/1f.jpg" alt="image2" />
                       <p className="legend">Image 2</p>
   
                   </div>
                   <div>
-                      <img src="/3.png" alt="image3"/>
+                      <img src="/1f.jpg" alt="image3"/>
                       <p className="legend">Image 3</p>
   
                   </div>
                   <div>
-                      <img src="/4.png" alt="image4"/>
+                      <img src="/1f.jpg" alt="image4"/>
                       <p className="legend">Image 4</p>
   
                   </div>
                   <div>
-                      <img src="/5.png" alt="image5"/>
+                      <img src="/1f.jpg" alt="image5"/>
                       <p className="legend">Image 5</p>
   
                   </div>
